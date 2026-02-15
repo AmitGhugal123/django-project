09637019f28dd7025f4b1b3506afe27d9746320a
Amit Ghugal <amitghugal1512@gmail.com>
2026-02-15 15:03:46 +0530

    setting of models.py file and refelect it on admin.py

--- newone/firstone/models.py
+++ newone/firstone/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Student(models.Model):
+
+    COURSE_CHOICES = [
+        ('CS', 'Computer Science'),
+        ('IT', 'Information Tech'),
+        ('ME', 'Mechanical'),
+        ('CE', 'Civil'),
+    ]
+
+    name = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+    course = models.CharField(max_length=2, choices=COURSE_CHOICES)
+
+    profile_pic = models.ImageField(upload_to='students/', blank=True, null=True)
+
+    age = models.IntegerField()
+    joined_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} - {self.course}"
+
--- newone/run.md
+++ newone/run.md
@@ -1,16 +1,17 @@
 ## ▶️ Run Project (Every Time You Open)
 
+### 🖥️Terminal 2 → Django
+```bash
+.venv\Scripts\activate
+cd newone
+python manage.py runserver
+```
+
 ### 🖥️ Terminal 1 — Tailwind
 
 ```bash
-venv\Scripts\activate
+.venv\Scripts\activate
 cd newone
 python manage.py tailwind start
 ```
 
-### 🖥️Terminal 2 → Django
-```bash
-venv\Scripts\activate
-cd newone
-python manage.py runserver
-```
